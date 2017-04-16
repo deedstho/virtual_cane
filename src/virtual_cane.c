2a6c91fd3072ffd1874c1aaa3af1f654615c05e1
Thomas Deeds <tomdeeds@0587385404.wireless.umich.net>
2017-04-16 14:17:10 -0400

    changed lidar wakeup delay to 1.5m from 1m

--- src/virtual_cane.c
+++ src/virtual_cane.c
@@ -20,6 +20,7 @@ static uint8_t bool_service_EINT3 = 0;
 static uint8_t bool_service_systick = 0;
 static uint8_t pwm_disabled = 0;
 
+// INITIALIZE ACCELEROMETER INTERRUPT PIN
 void initialize_sleep_interrupt(void){
 	uint32_t pin = 0x00020000;	//H[12] & P0[17]
 	//enable GPIO pin as input
@@ -35,53 +36,7 @@ void initialize_sleep_interrupt(void){
 	NVIC_EnableIRQ(EINT3_IRQn);
 }
 
-//void service_EINT3(void)
-//{
-////	// Disable Systick
-////	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
-//
-//	// clear pending interrupts from this source
-//	NVIC_ClearPendingIRQ(EINT3_IRQn);
-//
-//	//Chip_GPIO_SetPinState(LPC_GPIO, 0, 18, 1);
-//
-//	// go to sleep
-//	if(Chip_GPIOINT_GetStatusRising(LPC_GPIOINT, GPIOINT_PORT0) == 0x00020000){
-//		Chip_GPIOINT_ClearIntStatus(LPC_GPIOINT, 0, 0x00020000);
-//
-//		// Disable Systick
-//		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
-//
-//		//Chip_GPIO_SetPinState(LPC_GPIO, 0, 18, 1);// DEBUG
-//		haptic_standby_mode();
-//		lidar_sleep();
-//
-//		//haptic_sleep = 1;
-//	}
-//
-//	// wake up
-//	else if(Chip_GPIOINT_GetStatusFalling(LPC_GPIOINT, GPIOINT_PORT0) == 0x00020000){
-//		Chip_GPIOINT_ClearIntStatus(LPC_GPIOINT, 0, 0x00020000);
-//
-//		//Chip_GPIO_SetPinState(LPC_GPIO, 0, 18, 0);// DEBUG
-//
-//		lidar_wake();
-//		haptic_playback_mode();
-//
-//		// Enable Systick
-//		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
-//
-//		//haptic_sleep = 0;
-//	}
-//}
-
-//Chip_GPIO_SetPinState(LPC_GPIO, 0, 18, 0);
-
-//void EINT3_IRQHandler(void)
-//{
-//	bool_service_EINT3 = 1;
-//}
-
+// ACCELEROMETER INTERRUPT HANDLER
 void EINT3_IRQHandler(void)
 {
 	// Disable Systick
@@ -118,7 +73,7 @@ void EINT3_IRQHandler(void)
 
 		// delay for lidar power up
 		volatile int j = 0;
-		for(j = 0; j < 10000000; ++j);
+		for(j = 0; j < 15000000; ++j);
 
 		haptic_playback_mode();
 		// PWM wake just happens //
@@ -129,55 +84,7 @@ void EINT3_IRQHandler(void)
 	}
 }
 
-//void service_systick(void)
-//{
-//	//	// *DEBUG* set H[53] high (for oscilloscope)
-//	//	Chip_GPIO_SetPinState(LPC_GPIO, SCOPE_PORT, SCOPE_PIN, PIN_HIGH);
-//	//
-//		// wait until lidar is ready
-//	//	while( lidar_status() ){}
-//
-//		// *DEBUG* blink LED
-//		Board_LED_Toggle(0);
-//
-//	//	// read lidar distance
-//		uint16_t distance;
-//		distance = lidar_read();
-//	//
-//	//	// *DEBUG* print distance to uart
-//	//	//printf("%i\n", distance);
-//
-//		// output pitch based on distance
-//		distance_to_sound( distance );
-//
-//		// output vibration amplitude based on distance (correct for lidar distance offset)
-//		if (distance < 255) //(3 feet)
-//		{
-//			//haptic_notify();
-//			if(distance < 20) haptic_intensity(255);
-//			else haptic_intensity( 255 - distance );
-//		}
-//		else
-//		{
-//			haptic_intensity(0);
-//		}
-//
-//		// request a measurement for the next tick
-//		lidar_request();
-//	//
-//	//	// *DEBUG* set H[53] low (for oscilloscope)
-//	//	Chip_GPIO_SetPinState(LPC_GPIO, SCOPE_PORT, SCOPE_PIN, PIN_LOW);
-//}
-
-/**
- * @brief	Handle interrupt from SysTick timer
- * @return	Nothing
- */
-//void SysTick_Handler(void)
-//{
-//	bool_service_systick = 1;
-//}
-
+// SYSTICK HANDLER
 void SysTick_Handler(void)
 {
 	// Disable EINT3
